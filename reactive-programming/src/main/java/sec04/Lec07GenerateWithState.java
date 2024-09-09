129f4d9b8ff5592572ffc3a0d9d3ec802b93ed90
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-09-09 14:45:15 -0300

    sec04 Lec07GenerateWithState

---
+++ reactive-programming/src/main/java/sec04/Lec07GenerateWithState.java
@@ -0,0 +1,30 @@
+package sec04;
+
+import common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec07GenerateWithState {
+
+	public static void main(String[] args) {
+		/*
+		Flux.generate() aceita 3 parâmetro.
+		No caso abaixo o primeiro parâmetro é o valor 0 que passa a se chamar counter no segundo parametro
+		e é retornado gerando um loop até que uma condição seja atendida.
+		Um caso de uso para isso seria abrir uma conexão com banco de dados como primeiro parametro,
+		gerar um fluxo de dados e no terceiro parametro ou até que uma condição seja atendidade fechar a conexão.
+		 */
+		Flux.generate(
+				() -> 0,
+				(counter, sink) -> {
+					var country = Util.faker().country().name();
+					sink.next(country);
+					counter++;
+					if(counter == 10 || country.equalsIgnoreCase("brazil")){
+						sink.complete();
+					}
+					return counter;
+				}
+				)
+				.subscribe(Util.subscriber());
+	}
+}
